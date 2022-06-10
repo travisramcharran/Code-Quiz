3ceb7f5a69363749d279b4ccc3b6ea1fc08407fd
Travis Ramcharran <tc.ramcharran@gmail.com>
2022-06-10 04:26:24 -0400

    changed html, added js, changed css

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-var btnStart = document.getElementById("buttonStart");
-console.log(btnStart);
-btnStart.addEventListener("click",function(){
-
-title.innerText = "Game Sart: What is your name?"
-});
-
-
-var nextBtn = document.getElementById("nextBtn");
-console.log(nextBtn);
- var title = document.getElementById("headtitle");
- console.log(title)
-
- nextBtn.addEventListener("click",changerTitre)
- function changerTitre(){
-     title.innerText ="je suis Gisele"
- }
-    
+var quizQuestions = document.getElementById("quiz-questions");
+var btnStart = document.getElementById("btn-start");
+btnStart.addEventListener("click", starQuiz);
+function starQuiz(){
+    btnStart.classList.add("d-none")
+    quizQuestions.classList.remove("d-none")
+}    
